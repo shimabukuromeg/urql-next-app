615d5d78535bed2e95a58672c809c9d804a01d84
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-08-11 12:56:16 +0900

    mui導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+
+const theme = createTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
+import { Box, Button, Typography } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
-    <div>hello</div>
+    <Box sx={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <Typography variant="h2">TOPページ</Typography>
+      <Button variant="contained" color="primary">
+        ボタン
+      </Button>
+    </Box>
   )
 }
 
