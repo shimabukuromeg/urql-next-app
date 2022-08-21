77ea2343dd8203e03e42174ed3e417ad29ac397b
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-08-22 01:17:47 +0900

    refocusExchange を追加した

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -1,7 +1,11 @@
-import { createClient } from 'urql';
+import { createClient, dedupExchange, cacheExchange, fetchExchange } from 'urql';
+import { refocusExchange } from '@urql/exchange-refocus';
 
 export const client = createClient({
   url: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:54321/graphql/v1',
+  // @ts-ignore
+  exchanges: [dedupExchange, refocusExchange(), cacheExchange, fetchExchange],
+  requestPolicy: 'cache-first',
   fetchOptions: () => {
     const token = process.env.NEXT_PUBLIC_ANON_KEY;
     return {
